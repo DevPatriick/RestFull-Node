fd721698cb53a444b1c0fc95070ba0f73c390811
Patrick Reis Andrade <andrade.patrickreis@gmail.com>
2025-01-29 14:06:58 -0300

    AULA N08 CARREGANDO ROTAS COM O CONSIGN -  Instalei o consign no projeto, agora no meu arquivo de entrada 'index.js' ao inves de incluir cada nova rota com o consigo eu digo a pasta que ele deve incluir e onde ele deve usar, com estas alterações tive que mudar os arquivos das minhas rotas, não estou chamando mais o express nem o Router, eu exporto direto a função que recebe o app com os gets, e também troco a varivel do get, agora ela é o app. Com isso eu perco o recurso de deixar um pedaço da minha rota no index.js como era antes, com isso criei uma variavel dentro da função chamada routesUsers e ela recebe '/users' assim posso usar ela como padrão quando for criar uma nova rota, concatenando com o restante da rota.

--- readme.md
+++ readme.md
@@ -1,14 +1,14 @@
 # **Curso Node.js (Projeto em Andamento)**
 
-Este é o acompanhamento do progresso no curso de Node.js. O curso tem um total de **16 aulas**, e atualmente estou na **aula 7**. Aqui você pode ver o progresso e as etapas concluídas até agora.
+Este é o acompanhamento do progresso no curso de Node.js. O curso tem um total de **16 aulas**, e atualmente estou na **aula 8**. Aqui você pode ver o progresso e as etapas concluídas até agora.
 
 ---
 
 ## **Status do Curso**
 
 ### Progresso Atual  
-#### [███████░░░░░░░░░░░░░░░░░░] **43,75%**  
-- Aulas concluídas: **7 de 16**  
+#### [████████░░░░░░░░░░░░░░░] **50%**  
+- Aulas concluídas: **8 de 16**  
 
 ---
 
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,11 @@
-// Chamando o express
-const express = require('express');
-// routes recebe o express.Router() para as rotas
-const routes = express.Router()
+// esta função exporta a função que recebe o app como parametro e agora ao inves de ter
+// o routes eu passo o app.get 
+// com isso eu perco a função de deixar uma rota padrão no meu index.js
 
-
+module.exports = (app)=>{
 // agora eu não preciso criar o servidor o express mesmo cria
 // quando o navegador acessar a rota localhost:3000 vai fazer um get da informação
-routes.get('/',(req, res)=>{
+app.get('/',(req, res)=>{
     // aqui estou definindo status caso caia aqui 200 'ok'
     res.statusCode = 200
     // aqui estou informando qual tipo de conteudo o navegador vai receber
@@ -15,5 +14,4 @@ routes.get('/',(req, res)=>{
     // aqui é o corpo da resposta que o navegador vai receber
     res.end('<h1>Olá</h1>')
 });
-
-module.exports = routes;
+};
